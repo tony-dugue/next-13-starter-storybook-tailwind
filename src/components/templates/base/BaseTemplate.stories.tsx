56e8da799b527862334300af4af4412e44ebaa45
Tony Dugué <duguetony@gmail.com>
2023-04-10 17:00:10 +0200

    build: create BaseTemplate component

---
+++ src/components/templates/base/BaseTemplate.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import BaseTemplate, { IBaseTemplate } from './BaseTemplate';
+import { mockBaseTemplateProps } from './BaseTemplate.mocks';
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
+const meta: Meta<typeof BaseTemplate> = {
+  title: 'templates/BaseTemplate',
+  component: BaseTemplate,
+  //tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof BaseTemplate>;
+
+const args = {
+  ...mockBaseTemplateProps.base,
+} as IBaseTemplate;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export const Base: Story = {
+  args: {
+    ...mockBaseTemplateProps.base,
+  },
+};
