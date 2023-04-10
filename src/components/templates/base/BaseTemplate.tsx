8985aa8167ff3bed2cc7806031754532cdaa8ce2
Tony Dugué <duguetony@gmail.com>
2023-04-10 18:07:42 +0200

    feat: implement tailwind css / styled-components & storybook support

--- src/components/templates/base/BaseTemplate.tsx
+++ src/components/templates/base/BaseTemplate.tsx
@@ -1,9 +1,13 @@
+import tw from 'tailwind-styled-components';
+
 export interface IBaseTemplate {
   sampleTextProp: string;
 }
 
 const BaseTemplate: React.FC<IBaseTemplate> = ({ sampleTextProp }) => {
-  return <div>{sampleTextProp}</div>;
+  return <Title>{sampleTextProp}</Title>;
 };
 
 export default BaseTemplate;
+
+const Title = tw.h1`bg-gradient-to-r from-red-500 to-blue-500`;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,8 +4,6 @@ export default function Document() {
   return (
     <Html lang="fr">
       <Head>
-        <meta name="description" content="Portfolio de Tony Dugué" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* Favicon assets */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
