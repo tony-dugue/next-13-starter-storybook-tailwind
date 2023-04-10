7e57ab6224a1c95a60b65e6a1dc04b53a0ac7933
Tony Dugué <duguetony@gmail.com>
2023-04-10 17:19:02 +0200

    build: update _document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="fr">
+      <Head>
+        <meta name="description" content="Portfolio de Tony Dugué" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Favicon assets */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
